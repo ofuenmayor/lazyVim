c9428be650de644a293824666d81d8a67ce4d624
Omar Fuenmayor <omar@finalis.com>
2023-09-08 19:25:18 -0300

    first commit

---
+++ lua/plugins/test.lua
@@ -0,0 +1,29 @@
+return {
+  { "nvim-neotest/neotest-plenary" },
+  {
+    "nvim-neotest/neotest",
+    dependencies = { "haydenmeade/neotest-jest" },
+    keys = {
+      {
+        "<leader>tl",
+        function()
+          require("neotest").run.run_last()
+        end,
+        desc = "Run Last Test",
+      },
+    },
+    opts = function(_, opts)
+      table.insert(
+        opts.adapters,
+        require("neotest-jest")({
+          jestCommand = "yarn run test --",
+          jestConfigFile = "jest.config.ts",
+          env = { CI = true },
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        })
+      )
+    end,
+  },
+}
